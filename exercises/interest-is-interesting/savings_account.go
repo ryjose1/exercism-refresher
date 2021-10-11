5b73afc601b975ef749569bc792adca7529e05c0
Ryan <8942922+rajose@users.noreply.github.com>
2021-10-11 14:31:20 -0400

    Finished Savings Account exercise

---
+++ exercises/interest-is-interesting/savings_account.go
@@ -0,0 +1,42 @@
+package savings
+
+import "math"
+
+// InterestRate calculates the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+	switch {
+	case balance < 0:
+		return -3.213
+	case balance < 1000:
+		return 0.5
+	case balance < 5000:
+		return 1.621
+	case balance >= 5000:
+		return 2.475
+	default:
+		return 0
+	}
+}
+
+// InterestRate calculates the interest for the provided balance.
+func Interest(balance float64) float64 {
+	return math.Copysign(balance*float64(InterestRate(balance))/100.0, balance)
+}
+
+// AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
+func AnnualBalanceUpdate(balance float64) float64 {
+	return balance + Interest(balance)
+}
+
+// YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance:
+func YearsBeforeDesiredBalance(balance, targetBalance float64) (years int) {
+	if math.Signbit(balance) != math.Signbit(targetBalance) {
+		panic("Balance will never reach target due to differing signs")
+	}
+
+	for math.Abs(balance) < math.Abs(targetBalance) {
+		balance = AnnualBalanceUpdate(balance)
+		years += 1
+	}
+	return
+}
