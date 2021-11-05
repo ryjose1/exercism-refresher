df95ebc652e989f857520cd4cf3aff46dc07f7f8
Ryan <8942922+rajose@users.noreply.github.com>
2021-11-04 21:23:10 -0400

    Finished  Karls Languages exercise

--- java/karls-languages/src/main/java/LanguageList.java
+++ java/karls-languages/src/main/java/LanguageList.java
@@ -2,33 +2,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LanguageList {
-    private final List<String> languages = new ArrayList<>();
+  private final List<String> languages = new ArrayList<>();
 
-    public boolean isEmpty() {
-        throw new UnsupportedOperationException("Please implement the isEmpty() method");
-    }
+  public boolean isEmpty() {
+    return this.languages.isEmpty();
+  }
 
-    public void addLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the addLanguage() method");
-    }
+  public void addLanguage(String language) {
+    this.languages.add(language);
+  }
 
-    public void removeLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the removeLanguage() method");
-    }
+  public void removeLanguage(String language) {
+    this.languages.remove(language);
+  }
 
-    public String firstLanguage() {
-        throw new UnsupportedOperationException("Please implement the firstLanguage() method");
+  public String firstLanguage() {
+    if (!this.isEmpty()) {
+      return this.languages.get(0);
     }
+    return "";
+  }
 
-    public int count() {
-        throw new UnsupportedOperationException("Please implement the count() method");
-    }
+  public int count() {
+    return this.languages.size();
+  }
 
-    public boolean containsLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the containsLanguage() method");
-    }
+  public boolean containsLanguage(String language) {
+    return this.languages.contains(language);
+  }
 
-    public boolean isExciting() {
-        throw new UnsupportedOperationException("Please implement the isExciting() method");
-    }
+  public boolean isExciting() {
+    return this.containsLanguage("Java") || this.containsLanguage("Kotlin");
+  }
 }
