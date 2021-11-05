aa847f5253d6e35e033bbe3dbb83e2e7a369e13e
Ryan <8942922+rajose@users.noreply.github.com>
2021-11-04 22:35:45 -0400

    Finished Squeaky Clean exercise

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,25 @@
 class SqueakyClean {
-    static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+  private static final char ALPHA = '\u03B1';
+  private static final char OMEGA = '\u03C9';
+
+  static String clean(String identifier) {
+    StringBuilder s = new StringBuilder();
+
+    for (int i = 0; i < identifier.length(); i++) {
+      char c = identifier.charAt(i);
+      if(Character.isWhitespace(c)) {
+        s.append('_');
+      } else if (Character.isISOControl(c)) {
+        s.append("CTRL");
+      } else if (Character.isLetter(c) && (c < ALPHA || c > OMEGA)) {
+        s.append(c);
+      } else if (c == '-') {
+        if (i+1 < identifier.length() && Character.isLetter(identifier.charAt(i+1))){
+          s.append(Character.toUpperCase(identifier.charAt(++i)));
+        }
+      }
     }
+
+    return s.toString();
+  }
 }
