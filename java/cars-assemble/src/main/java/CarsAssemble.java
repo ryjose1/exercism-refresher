5a446ee6b93ce9c6c323f7112ab4822fba09be2d
Ryan <8942922+rajose@users.noreply.github.com>
2021-11-04 23:21:57 -0400

    Finished Cars Assemble Exercise

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,32 @@
+import java.util.HashMap;
+
 public class CarsAssemble {
 
-    public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.productionRateperHour() method");
-    }
+  private static final HashMap<Integer, Integer> successRate = new HashMap<>();
+  static {
+      successRate.put(1, 100);
+      successRate.put(2, 100);
+      successRate.put(3, 100);
+      successRate.put(4, 100);
+      successRate.put(5, 90);
+      successRate.put(6, 90);
+      successRate.put(7, 90);
+      successRate.put(8, 90);
+      successRate.put(9, 80);
+      successRate.put(10, 77);
+  }
 
-    public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.workingItemsPerMinute() method");
+  private static final int DEFAULT_CARS_PER_HOUR = 221;
+
+  public double productionRatePerHour(int speed) {
+    if(successRate.containsKey(speed)) {
+      return (double) DEFAULT_CARS_PER_HOUR * (double) successRate.get(speed) * (double) speed / 100;
+    } else {
+      return 0.0;
     }
+  }
+
+  public int workingItemsPerMinute(int speed) {
+    return (int) (productionRatePerHour(speed) / 60);
+  }
 }
